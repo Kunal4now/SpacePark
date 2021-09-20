061dd1e7259a1f7ad0dd996e329614aef59f737b
Kunal <kunal.rohitas29@gmail.com>
2021-09-20 22:16:42 +0530

    This is the project ive made so far for minor

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.set('view engine', 'ejs')
+
+//It basically tells our express app to use the public folder in the app
+app.use('/public', express.static('public'))
+
+app.get('/', (req, res) =>{
+  res.render('home-guest')
+})
+
+app.get('/profile', (req, res) => {
+  res.render('profile')
+})
+
+app.get('/404', (req, res) => {
+  res.render('404')
+})
+
+app.listen(3000)
+    console.log('listening on port http://localhost:3000/')
