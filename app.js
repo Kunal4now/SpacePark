6f99247f4ae350764be36f38f5e9c64604af7732
Kunal <kunal.rohitas29@gmail.com>
2021-09-26 17:28:40 +0530

    added env variables using dotenv package

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var path = require('path');
-
 var app = express();
+
+require('dotenv').config()
+const db = require('./db')
+
+const hostname = process.env.HOST;
+const port = process.env.PORT;
 
 //setting the view engine to ejs
 app.set('view engine', 'ejs')
@@ -25,5 +30,20 @@ app.get('/profile-new', (req, res) => {
   res.render('profile-new')
 })
 
-app.listen(3000)
-    console.log('listening on port http://localhost:3000/')
+app.get('/create-post', (req, res) => {
+  res.render('create-post')
+})
+
+app.get('/home-dashboard', (req, res) => {
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum facilis. Porro soluta et quas odio magnam ipsum! Ipsam, veritatis ab tempore commodi minima tenetur quasi illo quam nemo delectus quo nam beatae repudiandae assumenda consectetur veniam quidem voluptates et aperiam debitis vitae unde. Iusto natus quod minima voluptates eaque.'},
+    {title: 'Mario finds stars', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum facilis. Porro soluta et quas odio magnam ipsum! Ipsam, veritatis ab tempore commodi minima tenetur quasi illo quam nemo delectus quo nam beatae repudiandae assumenda consectetur veniam quidem voluptates et aperiam debitis vitae unde. Iusto natus quod minima voluptates eaque.'},
+    {title: 'How to defeate bowser', snippet: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, earum facilis. Porro soluta et quas odio magnam ipsum! Ipsam, veritatis ab tempore commodi minima tenetur quasi illo quam nemo delectus quo nam beatae repudiandae assumenda consectetur veniam quidem voluptates et aperiam debitis vitae unde. Iusto natus quod minima voluptates eaque.'}
+  ]
+
+  res.render('home-dashboard', {blogs})
+})
+
+
+app.listen(port)
+    console.log(`listening on port http://${hostname}:${port}/`)
