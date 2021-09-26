dccf401f5b3b1b1c9e149fd11f8bd1a7aab6d315
Kunal <kunal.rohitas29@gmail.com>
2021-09-26 17:25:15 +0530

    connected mongodb with app

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const URI = 'mongodb+srv://kunal:Idbrs078%40@cluster0.o8b6a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const URI = process.env.URI;
 
 mongoose.connect(URI).then((result) => console.log('connedted to db'));
 
