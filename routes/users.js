4d572c63d71ccf1a5ae79aba78de5dda5c3c1a3a
Kunal <kunal.rohitas29@gmail.com>
2021-09-28 21:24:42 +0530

    added user registration

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
-var router = express.Router();
+const bcrypt = require('bcrypt');
+var User = require('../models/User')
+const db = require('../db')
+const router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/register', async (req, res) => {
+  let errors = []
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    
+    if (req.body.password.length < 6) {
+      errors.push({msg: 'Password should contain atleast 6 characters'})
+    }
+    
+    //problem: Email already registered was not shown in error list
+    //Solution: findOne need to await for to get the work done so that
+    await User.findOne({email: req.body.email}).then(user => {
+      if (user) {
+        errors.push({msg: 'Email already registered'})
+        console.log(errors)
+      } 
+    });
+    console.log(errors);
+    if (errors.length > 0) {
+      res.render('home-guest', {errors})
+    } else {
+      user.save().then((result) => res.send(result)).catch((err) => console.log(err))
+    }
+  } catch(e) {
+    console.log(e)
+  }
+})
 
 module.exports = router;
