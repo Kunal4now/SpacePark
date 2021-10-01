16bfc3480ee950e7ed6a03584f407a6d9d2f1f16
Kunal <kunal.rohitas29@gmail.com>
2021-10-01 15:32:27 +0530

    added passport local strategy for authentication

---
+++ passport-config.js
@@ -0,0 +1,39 @@
+const bcrypt = require("bcrypt");
+const User = require("./models/User");
+const LocalStrategy = require("passport-local").Strategy;
+
+function initialize(passport) {
+  const authenticateUser = (email, password, done) => {
+    console.log(email);
+    User.findOne({ email: email })
+      .then(async (user) => {
+        if (!user) {
+          console.log("errr");
+          return done(null, false, { message: "No user with that email" });
+        }
+        if (await bcrypt.compare(password, user.password)) {
+          return done(null, user, { message: "Sucess" });
+        } else {
+          return done(null, false, { message: "Password incorrect" });
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  passport.use(
+    new LocalStrategy(
+      { usernameField: "loginEmail", passwordField: "loginPassword" },
+      authenticateUser
+    )
+  );
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+}
+
+module.exports = initialize;
+
+
