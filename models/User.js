362c7f144aad65caaeaf7cb3f8189db621d0dc18
Kunal <kunal.rohitas29@gmail.com>
2021-10-04 22:06:36 +0530

    added all fields to the user model

--- models/User.js
+++ models/User.js
@@ -1,8 +1,19 @@
 var mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
-    name: String
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
-const user = mongoose.model('user', userSchema);
-module.exports = user;
+const User = mongoose.model('User', userSchema);
+module.exports = User;
