49bba8bb31f79ff23711a5db86c9d0c07aa8cad5
Kunal <kunal.rohitas29@gmail.com>
2021-10-18 15:45:50 +0530

    user profile pic will be sent to frontend to show on the cards of their respective posts

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 const Post = require('../models/Post');
+const User = require('../models/User');
+const fs = require('fs')
 var router = express.Router();
 
 router.get('/', checkNotAuthenticated, (req, res) =>{
@@ -16,11 +18,20 @@ router.get('/404', (req, res) => {
 
 router.get('/home-dashboard', checkAuthenticated, async (req, res) => {
   try {
+    const profileUser = await User.findById(req.user.id).then((names) => {
+      return names
+    }).catch((err) => console.log(err))
+    var img = profileUser.profilePicture.data
+    if (typeof img == 'undefined') {
+      img = fs.readFileSync('public/default/default_profile_pic.png').toString('base64');
+    } else {
+      img = img.toString('base64')
+    }
     Post.find({}, (err, posts) => {
       if (err) {
         res.status(500).json("No post found")
       } else {       
-        res.render('home-dashboard', {blogs: posts, id: req.user.id})
+        res.render('home-dashboard', {blogs: posts, id: req.user.id, image:img})
       }
     }).sort({createdAt: 'desc'})
   } catch(err) {
@@ -31,15 +42,24 @@ router.get('/home-dashboard', checkAuthenticated, async (req, res) => {
 router.get('/profile-new/:id', checkAuthenticated, async (req, res) => {
   if (req.user.id === req.params.id) {
     try {
+      const profileUser = await User.findById(req.user.id).then((names) => {
+        return names
+      }).catch((err) => console.log(err))
+      var img = profileUser.profilePicture.data
+      if (typeof img == 'undefined') {
+        img = fs.readFileSync('public/default/default_profile_pic.png').toString('base64');
+      } else {
+        img = img.toString('base64')
+      }
       Post.find({userID: req.user.id}, (err, posts) => {
         if (err) {
           res.status(500).json("No post found")
         } else {       
-          res.render('profile-new', {blogs: posts, id: req.user.id})
+          res.render('profile-new', {blogs: posts, id: req.user.id, image: img})
         }
       }).sort({createdAt: 'desc'})
     } catch(err) {
-      res.status(500).json("Error")
+      res.status(500).json(err)
     }
   } else {
     return res.status(400).send({
