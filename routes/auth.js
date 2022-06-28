7d50e3aa39340c200f9b9cec7bfe41a541c13503
Kunal <kunal.rohitas29@gmail.com>
2022-06-28 12:44:20 +0530

    Fix bcrypt

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 var express = require("express");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 var User = require("../models/User");
 const fs = require('fs')
 const passport = require("passport");
